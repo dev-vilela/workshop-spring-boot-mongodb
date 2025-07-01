adc2bcb2d8d40c29a753d088cdf7ff035b3b6144
Paulo Vilela <paulov4@hotmail.com>
2025-07-01 19:25:06 -0300

    Obtendo um post por id

---
+++ src/main/java/com/springmongodb/main/resources/PostResource.java
@@ -0,0 +1,25 @@
+package com.springmongodb.main.resources;
+
+
+import com.springmongodb.main.domain.Post;
+import com.springmongodb.main.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id){
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+
+    }
+
+
+}
---
+++ src/main/java/com/springmongodb/main/services/PostService.java
@@ -0,0 +1,22 @@
+package com.springmongodb.main.services;
+
+import com.springmongodb.main.domain.Post;
+import com.springmongodb.main.repository.PostRepository;
+import com.springmongodb.main.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
